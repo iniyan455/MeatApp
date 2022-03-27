25765440fe2ee1a9585f0ced81c687a21feb2b48
Iniyan <Iniyan.m@rapdio.bike.com>
2022-03-27 21:56:40 +0530

    Gradle Dependency Dsl kts implementation

--- buildSrc/src/main/kotlin/App.kt
+++ buildSrc/src/main/kotlin/App.kt
@@ -1,3 +1,7 @@
 object App {
     const val meatProject = "com.iniyan.meat"
+    const val compose_version = "1.2.0-alpha06"
+    const val kotlin_version = "1.6.10"
+    const val gradle_version = "7.1.2"
+    const val gradle_plugin = "2.40.2"
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+
+rootProject.name = ("Meat")
+include(":app")
+// include ":navigation"
+
+include(":feature:user:onboarding")
+include(":feature:user:home")
+include(":feature:user:profile")
+
+include(":common:utilities")
+include(":common:network")
+include(":common:datastore")
+include(":common:firebase")
+include(":common:base")
+include(":common:core")
